882787f92d7f8db7ee4379d0fa6f98fe8711ef81
Clément Sartori <clementsartori@gmail.com>
2019-05-02 12:22:32 +0200

    completed ecPV and ecSection

--- src/ecSection.ml
+++ src/ecSection.ml
@@ -142,7 +142,7 @@ let rec on_mpath_instr cb (i : instr)=
 
   | Sabstract _ -> ()
 
-  | Scost s -> assert false                                   (* TODO : FIXME *)
+  | Scost s -> on_mpath_stmt cb s                             (* TODO : FIXME *)
 
 and on_mpath_stmt cb (s : stmt) =
   List.iter (on_mpath_instr cb) s.s_node
